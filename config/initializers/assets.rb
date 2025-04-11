6a7af993a849340c4a3fb2498d936702f4cd1334
haris767 <hariharis767@gmail.com>
2025-04-11 15:20:41 +0500

    visible icons

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,12 +4,7 @@
 Rails.application.config.assets.version="1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
-Rails.application.config.assets.paths << Rails.root.join("node_modules/bootstrap-icons/font")
-Rails.application.config.assets.paths << Rails.root.join("vendor", "assets")
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile +% admin.js admin.css)
-Rails.application.config.assets.precompile + %w[adminlte/dist/css/*.css adminlte/dist/js/*.js]
-Rails.application.config.assets.paths << Rails.root.join("app", "assets", "fonts")
-Rails.application.config.assets.precompile += %w[ webfonts/* ]
